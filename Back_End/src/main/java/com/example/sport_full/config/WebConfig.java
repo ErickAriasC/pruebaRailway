3db846eef18164586a055bfe79530c08fc610175
erick cardona <eariascardona0@soy.sena.edu.co>
2024-11-13 14:01:17 -0500

    11

--- Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
+++ Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
@@ -9,10 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://prueba-railway.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+        registry.addMapping("/*").allowedOrigins("https://prueba-railway.vercel.app").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
 
     }
 }
