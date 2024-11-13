33d1ecde1a93d5baedc38f5953327004e602ed56
erick cardona <eariascardona0@soy.sena.edu.co>
2024-11-13 16:50:00 -0500

    14

--- Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
+++ Back_End/src/main/java/com/example/sport_full/config/WebConfig.java
@@ -2,18 +2,19 @@ package com.example.sport_full.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://prueba-railway.vercel.app/")
+                .allowedOrigins("https://prueba-railway.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
-
 
     }
 }
